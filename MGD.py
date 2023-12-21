f6bdcacbca031dca1490cc881c85bf3972b84630
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2023-12-21 11:55:53 +0700

    Update MGD.py

--- MGD.py
+++ MGD.py
@@ -1,80 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Generate random data
+np.random.seed(0)
+X1 = np.random.randn(100, 2) + np.array([-2, -2])
+X2 = np.random.randn(100, 2) + np.array([2, 2])
+X = np.vstack((X1, X2))
+y = np.concatenate((np.zeros(100), np.ones(100)))
+
+# Add bias term to feature matrix
+X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+# Define sigmoid function
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
+# Define cross-entropy loss function
 def cross_entropy_loss(y, y_hat):
     return -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
 
-def mini_batch_gradient_descent(X, y, theta, alpha, batch_size, num_epochs):
+# Define mini-batch gradient descent function
+def mini_batch_gradient_descent(X, y, alpha, batch_size, num_epochs):
+    theta = np.zeros(X.shape[1])  # Initialize parameters
+    losses = []  # Track loss values during training
+
     for epoch in range(num_epochs):
-        for i in range(0, len(X), batch_size):
+        for i in range(0, X.shape[0], batch_size):
             X_batch = X[i:i+batch_size]
             y_batch = y[i:i+batch_size]
 
             y_hat = sigmoid(X_batch.dot(theta))
-            J = cross_entropy_loss(y_batch, y_hat)
-
+            loss = cross_entropy_loss(y_batch, y_hat)
             gradient = X_batch.T.dot(y_hat - y_batch) / batch_size
 
             theta -= alpha * gradient
 
-    return theta
+        losses.append(loss)
 
-def plot_decision_boundary(X, y, theta):
-    # Get the minimum and maximum values of the features
-    minX = np.min(X[:, 0])
-    maxX = np.max(X[:, 0])
-    minY = np.min(X[:, 1])
-    maxY = np.max(X[:, 1])
+    return theta, losses
 
-    # Create a grid of points
-    xx, yy = np.meshgrid(np.linspace(minX, maxX, 200), np.linspace(minY, maxY, 200))
-
-    # Flatten the grid
-    X_grid = np.c_[xx.ravel(), yy.ravel()]
-
-    # Predict the labels for the grid points
-    y_hat = sigmoid(X_grid.dot(theta))
-
-    # Reshape the predictions back into a 2D array
-    y_hat = y_hat.reshape(xx.shape)
-
-    # Plot the decision boundary
-    plt.contour(xx, yy, y_hat, levels=[0.5], colors='black')
-
-    # Plot the data points
-    plt.scatter(X[:, 0], X[:, 1], c=y)
-
-    # Set the labels and title
-    plt.xlabel('X1')
-    plt.ylabel('X2')
-    plt.title('Decision Boundary')
-
-    # Show the plot
-    plt.show()
-
-# Generate two random data sets
-X1 = np.random.rand(100, 2)
-X2 = np.random.rand(100, 2) + 2
-
-# Combine the data sets
-X = np.vstack((X1, X2))
-
-# Create labels for the data sets
-y = np.concatenate((np.zeros(100), np.ones(100)))
-
-# Initialize the parameters
-theta = np.zeros(X.shape[1])
-
-# Set the hyperparameters
+# Set hyperparameters
 alpha = 0.01
 batch_size = 32
-num_epochs = 1000
+num_epochs = 100
 
 # Train the model
-theta = mini_batch_gradient_descent(X, y, theta, alpha, batch_size, num_epochs)
+theta, losses = mini_batch_gradient_descent(X, y, alpha, batch_size, num_epochs)
 
-# Visualize the decision boundary
-plot_decision_boundary(X, y, theta)
+# Plot the decision boundary
+x_values = np.linspace(-6, 6, 100)
+y_values = -(theta[0] + theta[1] * x_values) / theta[2]
+
+# Plot the data points
+plt.scatter(X1[:, 0], X1[:, 1], label='Class 0')
+plt.scatter(X2[:, 0], X2[:, 1], label='Class 1')
+
+# Plot the decision boundary line
+plt.plot(x_values, y_values, color='red', label='Decision Boundary')
+
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('Binary Classification with Mini-Batch Gradient Descent')
+plt.legend()
+plt.show()
+
